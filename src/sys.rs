28f33de85413d6318dd069beb293504c60107290
Joshua Narayana <europajuice@gmail.com>
2020-02-08 19:07:21 -0700

    deoptimized for correctness

--- src/sys.rs
+++ src/sys.rs
@@ -7,18 +7,7 @@ impl<'a, T: Component + Clone + Send + Sync> System<'a> for CollisionSystem<T> {
     type SystemData = (ReadStorage<'a, Shape>, ReadStorage<'a, Transform>, ReadStorage<'a, T>, WriteStorage<'a, Collisions<T>>);
 
     fn run(&mut self, (shapes, transforms, tags, mut collisions): Self::SystemData) {
-        let mut colcache: Vec<(&Shape, Collision<T>)> = Vec::new();
         for (i, (shapea, transa, taga, outputa)) in (&shapes, &transforms, &tags, &mut collisions).join().enumerate() {
-            {
-                let mut i = 0;
-                while i < colcache.len() {
-                    let shape = colcache[i].0;
-                    if shapea as *const Shape == shape as *const Shape {
-                        outputa.add(colcache.remove(i).1);
-                    }
-                    i += 1;
-                }
-            }
             for (shapeb, transb, tagb) in (&shapes, &transforms, &tags).join().skip(i+1) {
                 if shapea as *const Shape == shapeb as *const Shape {
                     continue;
@@ -28,10 +17,6 @@ impl<'a, T: Component + Clone + Send + Sync> System<'a> for CollisionSystem<T> {
                         resolve: res,
                         tag: tagb.clone()
                     });
-                    colcache.push((shapeb, Collision{
-                        resolve: res * -1.0,
-                        tag: taga.clone(),
-                    }));
                 }
             }
         }
